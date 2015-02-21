1ff1265333d3592de83fd6ae55518b362c36e230
Terrasque <terrasque@thelazy.net>
2015-02-21 03:19:14 +0100

    Simple setup.py file included, and gitignore build/ dir

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='gw2lib',
+      version='0.1',
+      packages=['gw2lib'],
+      description='Guild Wars 2 Mumble and Web APIs',
+      author=u"Terrasque"
+      )
