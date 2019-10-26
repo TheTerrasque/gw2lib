16c121fad0664d643fe39639501b45928c1d169f
Xorgon <norman.reggox@gmail.com>
2019-10-26 16:23:05 +0100

    Updated to Python 3

--- gw2lib/webapi.py
+++ gw2lib/webapi.py
@@ -1,12 +1,15 @@
 import json
-import urllib, urllib2
-from common import Struct, SimpleCache
 import logging
-import types
+import urllib.error
+import urllib.parse
+import urllib.request
+
+from .common import Struct, SimpleCache
 
 L = logging.getLogger("gw2lib.webapi")
 
 API_CACHE = SimpleCache()
+
 
 def structify(obj):
     if isinstance(obj, dict):
@@ -17,39 +20,40 @@ def structify(obj):
 
 
 def is_iterable(obj):
-    return not isinstance(obj, types.StringTypes) and hasattr( obj, '__iter__')
+    return not isinstance(obj, (str,)) and hasattr(obj, '__iter__')
+
 
 class SimpleClient(object):
     # http://wiki.guildwars2.com/wiki/API:Main
-    
+
     BASE_URL = "https://api.guildwars2.com/"
-    return_struct=True
-    
+    return_struct = True
+
     def _create_kwargs(self, kwargs):
         temp = {}
-        for k, v in kwargs.items():
+        for k, v in list(kwargs.items()):
             if is_iterable(v):
-                v = ",".join((unicode(x) for x in v))
+                v = ",".join((str(x) for x in v))
             temp[k] = v
-        return "?" + urllib.urlencode(temp)
-    
+        return "?" + urllib.parse.urlencode(temp)
+
     def _get_url(self, args, kwargs):
-        url = self.BASE_URL + self.version + self.root + (args and "/" + "/".join(unicode(asd) for asd in args) or "")
+        url = self.BASE_URL + self.version + self.root + (args and "/" + "/".join(str(asd) for asd in args) or "")
         if kwargs:
             url = url + self._create_kwargs(kwargs)
         return url
-    
-    def __init__(self, version="v1", root = "", as_struct = True):
+
+    def __init__(self, version="v1", root="", as_struct=True):
         self.root = root
         self.version = version
         self.return_struct = as_struct
 
     def dict(self):
-        return SimpleClient(version = self.version, root = self.root, as_struct=False)
-        
+        return SimpleClient(version=self.version, root=self.root, as_struct=False)
+
     def __getattr__(self, attrib):
-        return SimpleClient(version = self.version, root = self.root + "/" + attrib, as_struct=self.return_struct)
-    
+        return SimpleClient(version=self.version, root=self.root + "/" + attrib, as_struct=self.return_struct)
+
     def get_help_url(self):
         """
         Guesstimates the gw2 wiki help page for this API endpoint
@@ -57,21 +61,21 @@ class SimpleClient(object):
         url = self._get_url([], {})
         url = url.replace(self.BASE_URL + "v", "http://wiki.guildwars2.com/wiki/API:")
         return url
-    
+
     def _fetch_url(self, url):
-        return urllib2.urlopen(url).read()
-        
+        return urllib.request.urlopen(url).read()
+
     def __call__(self, *args, **kwargs):
         url = self._get_url(args, kwargs)
         L.debug("URL: %s", url)
-        
+
         d = API_CACHE.get(url)
         if d == None:
             d = self._fetch_url(url)
             API_CACHE.set(url, d)
-            
+
         r = json.loads(d)
-        
+
         if self.return_struct:
             r = structify(r)
         return r
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(name='gw2lib',
-      version='0.1',
       packages=['gw2lib'],
+      version='0.2',
       description='Guild Wars 2 Mumble and Web APIs',
       author=u"Terrasque",
       url="https://github.com/TheTerrasque/gw2lib"
